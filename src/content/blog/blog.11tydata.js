683eabb64c659b5bdab1302217ca040a4a6793a4
Alex Zappa <alex@zappa.dev>
2024-06-30 21:27:36 +0100

    Add permalink generation with slugify
    The changes include the usage of "slugify" to generate permalinks for both the docs and blog pages. That will ensure a more SEO-friendly URL structure. The page file's slug will be transformed to a lowercase string that will be used in the URL.

--- src/content/blog/blog.11tydata.js
+++ src/content/blog/blog.11tydata.js
@@ -1,4 +1,9 @@
+import slugify from "slugify";
+
 export default {
   tags: ["posts"],
-  layout: "post.njk"
+  layout: "post.njk",
+  permalink: function({page}) {
+    return `/blog/${ slugify(page.fileSlug, { lower: true }) }/`
+  }
 };

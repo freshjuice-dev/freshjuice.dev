86854dc09cee3450066f2703cfc73c0692656281
Alex Zappa <alex@zappa.dev>
2024-08-12 00:45:25 +0200

    Add search functionality and shortcuts to docs section
    Integrated search functionality in the docs section with keyboard shortcuts (⌘K or Ctrl+K for opening, ESC for closing). Refactored script loading approach and added methods for toggling menu and search visibility.

--- src/assets/js/modules/Alpine.data/DOM.js
+++ src/assets/js/modules/Alpine.data/DOM.js
@@ -1,5 +1,5 @@
+/* global loadScript loadStylesheet PagefindUI */
 import debugLog from "../_debugLog";
-import loadScript from "../_loadScript";
 
 export default () => {
   return {
@@ -7,7 +7,68 @@ export default () => {
       dark: true,
       name: "dark",
     },
-
+    shortcuts: {
+      search: navigator.platform.toUpperCase().indexOf('MAC') >= 0 ? "⌘K" : "Ctrl+K",
+    },
+    docs: {
+      showOverlay: false,
+      showSearch: false,
+      drawerMenuOpen: false,
+      pagefind: null,
+      toggleDrawerMenu() {
+        debugLog("docsSearch: Toggle drawer menu");
+        this.showOverlay = !this.showOverlay;
+        this.drawerMenuOpen = !this.drawerMenuOpen;
+      },
+      toggleSearch() {
+        this.loadAssets();
+        this.showSearch = !this.showSearch;
+        this.showOverlay = !this.showOverlay;
+      },
+      openSearch() {
+        this.loadAssets();
+        this.showSearch = true;
+        this.showOverlay = true;
+        let elm = document.querySelector(".pagefind-ui__search-input");
+        debugLog("docsSearch: Focus search input", elm);
+        // focus on elm
+        elm.focus();
+      },
+      loadAssets() {
+        debugLog("docsSearch: Loading search assets");
+        //loadStylesheet("/docs/pagefind/pagefind-ui.css");
+        loadScript("/docs/pagefind/pagefind-ui.js", "defer", () => {
+          debugLog("docsSearch: pagefind.js loaded");
+          this.pagefind = new PagefindUI({
+            element: "#docsSearch",
+            showSubResults: true,
+            pageSize: 5,
+            showImages: false,
+            excerptLength: 15,
+            resetStyles: false,
+            bundlePath: "/docs/pagefind/",
+            //debounceTimeoutMs: 500,
+          });
+        });
+      },
+      searchInit() {
+        debugLog("docsSearch: Init");
+        window.addEventListener("keydown", (e) => {
+          // add event listener for opening search with CMD/CTRL + K
+          if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+            e.preventDefault();
+            this.toggleSearch();
+          }
+          // add event listener for closing search with ESC
+          if (e.key === "Escape") {
+            e.preventDefault();
+            this.drawerMenuOpen = false;
+            this.showSearch = false;
+            this.showOverlay = false;
+          }
+        });
+      }
+    },
     getThemeName() {
       if (localStorage.getItem("theme")) {
         this.theme.name = localStorage.getItem("theme");
@@ -19,10 +80,8 @@ export default () => {
         this.theme.name = "dark";
         this.theme.dark = true;
       }
-
       return this.theme.dark ? "dark" : "light";
     },
-
     toggleTheme() {
       this.theme.dark = !this.theme.dark;
       this.theme.name = this.theme.dark ? "dark" : "light";
@@ -32,9 +91,6 @@ export default () => {
         this.theme.name
       );
     },
-
-    loadScript: (src, loading = "defer", callback, callbackForced) => loadScript(src, loading, callback, callbackForced),
-
     init() {
       debugLog("AlpineJS DOM init");
       document.documentElement.classList.remove("dark");
@@ -45,6 +101,10 @@ export default () => {
       window.addEventListener("storage", () => {
         this.getThemeName();
       });
+      // location path startes with /docs
+      if (location.pathname.startsWith("/docs")) {
+        this.docs.searchInit();
+      }
     },
   };
 };

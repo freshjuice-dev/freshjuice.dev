ce4f4d13b38409c9479b7140932af91ac32d2455
Alex Zappa <alex@zappa.dev>
2024-08-15 04:53:12 +0200

    Delete redundant DOM and tracker modules; consolidate main.js
    Removed src/assets/js/modules/Alpine.data/DOM.js and src/assets/js/modules/tracker.js. Merged their functions into main.js to streamline code management and improve maintainability.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,23 +1,220 @@
-/* global domReady loadScript loadStylesheet */
-import "./modules/tracker";
+/* global domReady loadScript loadStylesheet PagefindUI */
+import Plausible from "plausible-tracker";
+import colorScheme from "./modules/_detectColorScheme";
 
+import debugLog from "./modules/_debugLog";
 import Alpine from "alpinejs";
 import intersect from "@alpinejs/intersect";
 import collapse from "@alpinejs/collapse";
 
-import dataDOM from "./modules/Alpine.data/DOM";
+
+const {trackEvent, trackPageview, enableAutoOutboundTracking} = Plausible({
+  domain: "freshjuice.dev",
+  apiHost: "https://firebird.beastful.org",
+});
+
 
 // The window.Alpine = Alpine bit is optional, but is nice to have for
 // freedom and flexibility. Like when tinkering with Alpine from the devtools for example.
 window.Alpine = Alpine;
 
+
 // Add plugins to Alpine
 Alpine.plugin(intersect);
 Alpine.plugin(collapse);
 
-Alpine.data("xDOM", dataDOM);
 
-// Start Alpine when the page is ready.
+Alpine.data("xDOM", () => {
+  return {
+    theme: {
+      dark: true,
+      name: "dark",
+    },
+    shortcuts: {
+      search: navigator.platform.toUpperCase().indexOf('MAC') >= 0 ? "⌘K" : "Ctrl+K",
+    },
+    docs: {
+      showOverlay: false,
+      showSearch: false,
+      drawerMenuOpen: false,
+      pagefind: null,
+      toggleDrawerMenu() {
+        debugLog("docsSearch: Toggle drawer menu");
+        this.showOverlay = !this.showOverlay;
+        this.drawerMenuOpen = !this.drawerMenuOpen;
+      },
+      toggleSearch() {
+        this.loadAssets();
+        this.showSearch = !this.showSearch;
+        this.showOverlay = !this.showOverlay;
+        if (this.showSearch) {
+          this.focusSearch();
+        } else {
+          this.blurSearch();
+        }
+      },
+      openSearch() {
+        this.loadAssets();
+        this.showSearch = true;
+        this.showOverlay = true;
+        this.focusSearch();
+      },
+      closeSearch() {
+        this.showSearch = false;
+        this.showOverlay = false;
+        this.blurSearch();
+      },
+      blurSearch() {
+        let input = document.querySelector(".pagefind-ui__search-input");
+        input.value = "";
+        input.blur();
+      },
+      focusSearch() {
+        setTimeout(() => {
+          let input = document.querySelector(".pagefind-ui__search-input");
+          input.focus();
+        }, 200);
+      },
+      loadAssets() {
+        debugLog("docsSearch: Loading search assets");
+        //loadStylesheet("/docs/pagefind/pagefind-ui.css");
+        loadScript("/docs/pagefind/pagefind-ui.js", "defer", () => {
+          debugLog("docsSearch: pagefind.js loaded");
+          this.pagefind = new PagefindUI({
+            element: "#docsSearch",
+            showSubResults: true,
+            pageSize: 100,
+            showImages: false,
+            excerptLength: 30,
+            resetStyles: false,
+            bundlePath: "/docs/pagefind/",
+            autofocus: true,
+            //debounceTimeoutMs: 500,
+            processResult: function (result) {
+              debugLog("docsSearch: Process result", result);
+              if (result.url === location.pathname) {
+                //js open link in same tab
+                result.url = location.pathname;
+                if (result.anchors) {
+                  result.anchors.forEach((anchor) => {
+                    anchor.url = `javascript:postMessage({type: 'closeSearch', anchor: '${anchor.id}'})`;
+                  });
+                }
+                if (result.sub_results) {
+                  result.sub_results.forEach((sub_result) => {
+                    sub_result.url = `javascript:postMessage({type: 'closeSearch', anchor: '${sub_result.url.split("#")[1]}'})`;
+                  });
+                }
+              }
+              return result;
+            }
+          });
+        });
+      },
+      searchInit() {
+        debugLog("docsSearch: Init");
+        window.addEventListener("message", (e) => {
+          if (e.data.type === "closeSearch") {
+            this.closeSearch();
+            const anchor = document.getElementById(e.data.anchor);
+            if (anchor) {
+              anchor.scrollIntoView();
+            }
+          }
+        });
+        window.addEventListener("keydown", (e) => {
+          // add event listener for opening search with CMD/CTRL + K
+          if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+            e.preventDefault();
+            this.toggleSearch();
+          }
+          // add event listener for closing search with ESC
+          if (e.key === "Escape") {
+            e.preventDefault();
+            this.drawerMenuOpen = false;
+            this.showSearch = false;
+            this.showOverlay = false;
+          }
+        });
+      }
+    },
+    getThemeName() {
+      if (localStorage.getItem("theme")) {
+        this.theme.name = localStorage.getItem("theme");
+        this.theme.dark = this.theme.name === "dark";
+      } else if (
+        window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+      ) {
+        this.theme.name = "dark";
+        this.theme.dark = true;
+      }
+      return this.theme.dark ? "dark" : "light";
+    },
+    toggleTheme() {
+      this.theme.dark = !this.theme.dark;
+      this.theme.name = this.theme.dark ? "dark" : "light";
+      localStorage.setItem("theme", this.theme.name);
+      document.documentElement.setAttribute(
+        "data-theme",
+        this.theme.name
+      );
+    },
+    init() {
+      debugLog("AlpineJS DOM init");
+      document.documentElement.classList.remove("dark");
+      document.documentElement.setAttribute(
+        "data-theme",
+        this.getThemeName()
+      );
+      window.addEventListener("storage", () => {
+        this.getThemeName();
+      });
+      // location path starts with /docs
+      if (location.pathname.startsWith("/docs")) {
+        this.docs.searchInit();
+      }
+    },
+  };
+});
+
+
 domReady(() => {
+
+  // track page view
+  trackPageview(
+    {},
+    {
+      props: {
+        title: document.title,
+        url: location.href,
+        path: location.pathname,
+        referrer: document.referrer,
+        prefersColorScheme: colorScheme(),
+        userAgent: navigator.userAgent,
+        deviceWidth: window.innerWidth,
+        project: "freshjuice.dev"
+      },
+    }
+  );
+
+  enableAutoOutboundTracking();
+
+  // track 404 page
+  if (document.body.classList.contains("page-404")) {
+    trackEvent("404", {
+      props: {
+        title: document.title,
+        url: location.href,
+        path: location.pathname,
+        referrer: document.referrer,
+        prefersColorScheme: colorScheme(),
+        userAgent: navigator.userAgent,
+        deviceWidth: window.innerWidth,
+      },
+    });
+  }
+
+  // Start Alpine when the page is ready.
   Alpine.start();
 });

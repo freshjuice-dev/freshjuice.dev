3a5da5f69f4bf37e9ec15e6552c06832095f7fad
Alex Zappa <alex@zappa.dev>
2024-08-15 02:02:07 +0200

    Remove Critical dependency and its usage
    Removed the Critical package and its related code from the project. This includes the 'critical' transformation in the 11ty config and the Critical package entry from package.json and package-lock.json.

--- cfg/_11ty/transforms.js
+++ cfg/_11ty/transforms.js
@@ -1,39 +1,6 @@
-import { generate as criticalGenerate } from 'critical';
 import { minify as minifyHTML } from "html-minifier";
 
 export default {
-
-  critical: async function (content, outputPath) {
-    if (
-      process.env.ELEVENTY_ENV === "production" &&
-      outputPath && outputPath.endsWith(".html")
-    ) {
-      try {
-        const { html } = await criticalGenerate({
-          base: `_site/`,
-          html: content,
-          inline: true,
-          extract: true,
-          // height: 1980,
-          // width: 1920,
-          dimensions: [
-            {
-              height: 896,
-              width: 414,
-            },
-            {
-              height: 1900,
-              width: 1920,
-            },
-          ]
-        });
-        return html;
-      } catch (err) {
-        console.error(err);
-      }
-    }
-    return content;
-  },
 
   minifyHTML: function (content, outputPath) {
     if (

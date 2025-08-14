382263ccf6930fd15876ae795dfe21fa535fb676
Alex Zappa <alex@zappa.dev>
2025-08-14 03:14:20 +0200

    Fix download URL to include "v" prefix and update file naming convention

--- _data/hsTheme.js
+++ _data/hsTheme.js
@@ -1,21 +1,25 @@
 async function getPackageVersion() {
   try {
-    const response = await fetch("https://raw.githubusercontent.com/freshjuice-dev/freshjuice-hubspot-theme/main/package.json");
+    const response = await fetch(
+      "https://raw.githubusercontent.com/freshjuice-dev/freshjuice-hubspot-theme/main/package.json",
+    );
     const data = await response.json();
     return data.version;
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
     return "v1.16.0"; // Default version in case of error
   }
 }
 
 async function getLatestReleaseVersion() {
   try {
-    const response = await fetch("https://api.github.com/repos/freshjuice-dev/freshjuice-hubspot-theme/releases");
+    const response = await fetch(
+      "https://api.github.com/repos/freshjuice-dev/freshjuice-hubspot-theme/releases",
+    );
     const data = await response.json();
     return data[0].tag_name;
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
     return "v1.16.0"; // Default version in case of error
   }
 }
@@ -27,7 +31,8 @@ export default async () => {
     gitRepo: "git@github.com:freshjuice-dev/freshjuice-hubspot-theme.git",
     demo: "https://143910617.hs-sites-eu1.com/blog",
     version: themeVersion,
-    releases: "https://github.com/freshjuice-dev/freshjuice-hubspot-theme/releases",
-    download: `https://github.com/freshjuice-dev/freshjuice-hubspot-theme/releases/download/${themeVersion}/freshjuice-hubspot-theme-${themeVersion}.zip`,
+    releases:
+      "https://github.com/freshjuice-dev/freshjuice-hubspot-theme/releases",
+    download: `https://github.com/freshjuice-dev/freshjuice-hubspot-theme/releases/download/v${themeVersion}/freshjuice-hubspot-theme-${themeVersion}-dev.zip`,
   };
-}
+};

afcc9516049d13662c8f79930b136dff47c028c1
Alex Zappa <alex@zappa.dev>
2025-09-21 20:49:20 +0200

    Replace `postcss-csso` with `cssnano` for CSS optimization and update related dependencies.

--- postcss.config.js
+++ postcss.config.js
@@ -2,13 +2,26 @@
 import nested from "postcss-nested";
 import tailwindcss from "@tailwindcss/postcss";
 import autoprefixer from "autoprefixer";
-import csso from "postcss-csso";
+import cssnano from "cssnano";
 
 export default {
   plugins: [
     nested(),
     tailwindcss(),
     autoprefixer(),
-    ...(process.env.ELEVENTY_ENV === "production" ? [csso()] : []),
+    ...(process.env.ELEVENTY_ENV === "production"
+      ? [
+          cssnano({
+            preset: [
+              "default",
+              {
+                discardComments: {
+                  removeAll: true,
+                },
+              },
+            ],
+          }),
+        ]
+      : []),
   ],
 };

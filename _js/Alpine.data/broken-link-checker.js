07fb314ff01bab1175327dc5364510531274ab80
zapalblizh <alexander@zapparov.dev>
2025-09-26 16:24:24 +0200

    refactored broken-link-checker to include URL sanitization, enhanced UI states, and refined error handling logic

--- _js/Alpine.data/broken-link-checker.js
+++ _js/Alpine.data/broken-link-checker.js
@@ -1,26 +1,69 @@
 /* global Alpine */
 import debugLog from "../modules/_debugLog";
+import { stripTags, allowHttpUrl } from "../modules/_sanitize";
 
 document.addEventListener("alpine:init", () => {
   Alpine.data("BrokenLinkChecker", () => ({
-    result: {},
     urls: "",
+    result: {},
+    isChecking: false,
+    buttonLabel: "Check Links",
     openIndex: -2,
     keysArray: [],
-
+    urlsStates: [],
+    state: "idle", // idle, checking, success, error
+    setButtonLabel() {
+      switch (this.state) {
+        case "loading":
+          this.buttonLabel = "Checking for Broken Links...";
+          break;
+        case "success":
+          this.buttonLabel = "Check More Links";
+          break;
+        case "error":
+          this.buttonLabel = "Please try with a different URL";
+          break;
+        default:
+          this.buttonLabel = "Check Links";
+      }
+    },
     getIndex(url) {
       return this.keysArray.indexOf(url);
     },
+    isValidUrl(url) {
+      let strippedUrl = stripTags(url);
+      return allowHttpUrl(strippedUrl);
+    },
 
-    state: "idle", // idle, checking, success, error
     async checkLinks(event) {
       debugLog("Checking links");
       event.preventDefault();
 
-      let checkList = this.urls.split("\n");
+      if (!this.urls.trim()) {
+        alert("Please enter at least one URL.");
+        return;
+      }
 
-      for (let link of checkList) {
-        try {
+      this.isChecking = true;
+      this.result = {};
+
+      const checkList = this.urls
+        .split("\n")
+        .filter((link) => link.trim() !== "");
+
+      checkList.forEach((link) => {
+        if (!this.isValidUrl(link)) {
+          throw new Error("Invalid URL format");
+        } else {
+          this.result[link] = {};
+          this.urlsStates.push("loading");
+        }
+      });
+
+      let i = 0;
+      let link = "";
+      try {
+        for (link of checkList) {
           const response = await fetch(
             "https://api.freshjuice.dev/broken-link-checker",
             {
@@ -33,21 +76,15 @@ document.addEventListener("alpine:init", () => {
           );
 
           this.result[link] = await response.json();
-
-          debugLog(this.result);
-        } catch (error) {
-          // TODO: revise this error handling
+          this.urlsStates[i] = "success";
+          i++;
         }
-      }
-
-      if (!this.urls.trim()) {
-        alert("Please enter at least one URL.");
-        return;
+      } catch (error) {
+        this.result[link] = {};
+        this.urlsStates[i] = "error";
       }
 
       this.keysArray = Object.keys(this.result);
-
-      this.state = "checking";
     },
     init() {
       debugLog("Broken Link Checker initialized");

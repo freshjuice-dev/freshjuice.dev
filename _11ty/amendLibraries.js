0a0cb24e2c8a616a09106eec22fdd88606dee517
Alex Zappa <alex@zappa.dev>
2025-10-31 08:11:12 +0100

    add: improve markdown anchor slugify logic and extend level depth

--- _11ty/amendLibraries.js
+++ _11ty/amendLibraries.js
@@ -5,15 +5,18 @@
 import markdownItAnchor from "markdown-it-anchor";
 
 export default {
-
   // Customize Markdown library settings:
   markdownItAnchor: (eleventyConfig) => {
     eleventyConfig.amendLibrary("md", (mdLib) => {
       mdLib.use(markdownItAnchor, {
-        level: [1, 2, 3, 4],
-        slugify: eleventyConfig.getFilter("slugify"),
+        level: [1, 2, 3, 4, 5],
+        slugify: (str) => {
+          const base = eleventyConfig.getFilter("slugify")(str);
+          let cleaned = base.replace(/^[0-9]+(?:[-_\s]+)?/, "");
+          cleaned = cleaned.replace(/^[-_\s]+/, "");
+          return cleaned || "section";
+        },
       });
     });
-  }
-
+  },
 };
